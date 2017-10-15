370c454e35714f303bf53da4e383fb4aa7e54697
Davi09 <davindersandhu@Davinders-MacBook-Pro.local>
2017-10-15 01:58:56 -0400

    added controllers=1

--- js/controllers/career.controller.js
+++ js/controllers/career.controller.js
@@ -2,17 +2,16 @@ var app=angular.module('TrueSolutions');
 app.controller('CareerController',function CareerController($scope,$http,$window,$location,$anchorScroll,$timeout,anchorSmoothScroll){
   $window.scrollTo(0,0);
   $scope.jobsData='';
-  $scope.currentJobDescription='';
+  $scope.currentJobDescription="";
   $scope.hideDescription=true;
   $http.get('../../assets/json/jobs.json').then(function(success){
     $scope.jobsData=success.data;
   });
 
   $scope.loadDescription=function(val){
-
+    $scope.hideDescription=true;
     if(val == $scope.currentJobDescription){
       console.log('true');
-      $scope.hideDescription=true;
       $scope.currentJobDescription="";
     }else{
     $scope.hideDescription=false;
